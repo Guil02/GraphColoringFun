196f46f25545052e79ed23ff718ec613884d8c96
guil <g.bams@student.maastrichtuniversity.nl>
2022-11-18 13:41:36 +0100

    went back to the old version of the greedy since the new one doesn't work

--- algorithms/Greedy.cpp
+++ algorithms/Greedy.cpp
@@ -3,7 +3,6 @@
 //
 
 #include "Greedy.h"
-#include <iostream>
 
 void Greedy::calculateColoring(Graph &g) {
     for (Node &n: g.nodes) {
@@ -12,11 +11,11 @@ void Greedy::calculateColoring(Graph &g) {
 }
 
 void Greedy::assignColor(Node &n) {
+    int color = 1;
     std::vector<int> adjacentColors = n.adjacentColors();
-    if (adjacentColors.empty()) {
-        n.setColor(1);
-    } else {
-        auto it = std::max_element(adjacentColors.begin(), adjacentColors.end());
-        n.setColor(it[0] + 1);
+    while (std::find_if(adjacentColors.begin(), adjacentColors.end(), [&color](int a) { return color == a; }) !=
+           adjacentColors.end()) {
+        color++;
     }
+    n.setColor(color);
 }
--- main.cpp
+++ main.cpp
@@ -5,10 +5,13 @@
 
 int main() {
     Graph g = Graph(5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
     g.addEdge(1, 4);
-    std::cout << g.hasEdge(4, 1);
-    std::cout << g.hasEdge(1, 4);
-    std::cout << g.hasEdge(1, 3);
+    g.addEdge(2, 3);
+    g.addEdge(2, 4);
 
     Node n = g.getNode(4);
     std::cout << n.id << "\n";
