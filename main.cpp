3a3ce99b047529f6a2c00e6d224a750167eab258
guil <g.bams@student.maastrichtuniversity.nl>
2022-11-18 10:04:02 +0100

    moved entire graph structure into separate directory

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include "Graph.h"
+#include "graph/Graph.h"
 
 
 int main() {
     Graph g = Graph(5);
     g.addEdge(1, 4);
-//    std::cout << g.hasEdge(4, 1);
-//    std::cout << g.hasEdge(1, 4);
-//    std::cout << g.hasEdge(1, 3);
+    std::cout << g.hasEdge(4, 1);
+    std::cout << g.hasEdge(1, 4);
+    std::cout << g.hasEdge(1, 3);
 
     Node n = g.getNode(4);
     std::cout << n.id;
+
+
 }
